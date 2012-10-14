4d7b6b082fde1e580e02de01d74f01059dfec713
Syohei YOSHIDA <syohex@gmail.com>
2012-10-14 18:31:39 +0900

    Checking in changes prior to tagging of version 0.03.
    Changelog diff is:

--- lib/WebService/DMM.pm
+++ lib/WebService/DMM.pm
@@ -23,7 +23,7 @@ use WebService::DMM::Series;
 
 use utf8;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 my $agent_name = __PACKAGE__ . "/$VERSION";
 our $UserAgent = Furl->new(agent => $agent_name);
