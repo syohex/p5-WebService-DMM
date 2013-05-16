d36c6ffc03d5debbe124039aff84828fb70a2125
Syohei YOSHIDA <syohex@gmail.com>
2013-05-16 23:48:14 +0900

    Checking in changes prior to tagging of version 0.06.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index dff4383..a84e6a7 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,6 +1,8 @@
     Revision history for Perl extension WebService::DMM
    
     {{$NEXT}}
    +
    +0.06 2013-05-16T14:47:59Z
             - implement 'last_response' method
               'last_response' returns a last response which is Furl::Response
               instance.

--- lib/WebService/DMM.pm
+++ lib/WebService/DMM.pm
@@ -23,7 +23,7 @@ use WebService::DMM::Series;
 
 use utf8;
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 my $agent_name = __PACKAGE__ . "/$VERSION";
 our $UserAgent = Furl->new(agent => $agent_name);
